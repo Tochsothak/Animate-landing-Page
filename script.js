d3ce8767e432bb022d7139e0ed9051524de91c8e
Tochsothak <tochsothak882@gmail.com>
2024-09-15 16:04:07 +0700

    add responsive  design for mobile devices

--- script.js
+++ script.js
@@ -1,39 +1,52 @@
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-// Element to animate
+AOS.init();
 
-const heroHeading = document.querySelector(".hero h1");
-const heroParagraph = document.querySelector(".hero p");
-const heroButton = document.querySelector(".hero .btn");
-const featureList = document.querySelector(".feature-list");
-// console.log(heroHeading, heroParagraph, heroButton, featureList);
-
-//Scroll Events Listener
-window.addEventListener("scroll", () => {
-    //Animate hero content
-    if (isInViewport(heroHeading)) {
-        heroHeading.style.opacity = "1";
-        heroHeading.style.tranform = "translateY(0)";
-    }
-    if (isInViewport(heroParagraph)) {
-        heroParagraph.style.opacity = "1";
-        heroParagraph.style.transform = "translateY(0)";
-    }
-    if (isInViewport(heroButton)) {
-        heroButton.style.opacity = "1";
-        heroButton.style.transform = "transalteY(0)";
-    }
-
-    //Animate features
-    if (isInViewport(featureList)) {
-        featureList.style.opacity = "1";
-    }
+// Smooth scrolling for navigationbar
+const navbar = document.querySelectorAll(".navbar a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute("href");
+    document.querySelector(targetId).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
 });
+
+// function isInViewport(element) {
+//   const rect = element.getBoundingClientRect();
+//   return (
+//     rect.top >= 0 &&
+//     rect.left >= 0 &&
+//     rect.bottom <=
+//       (window.innerHeight || document.documentElement.clientHeight) &&
+//     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//   );
+// }
+// // Element to animate
+
+// const heroHeading = document.querySelector(".hero h1");
+// const heroParagraph = document.querySelector(".hero p");
+// const heroButton = document.querySelector(".hero .btn");
+// const featureList = document.querySelector(".feature-list");
+// // console.log(heroHeading, heroParagraph, heroButton, featureList);
+
+// //Scroll Events Listener
+// window.addEventListener("scroll", () => {
+//   //Animate hero content
+//   if (isInViewport(heroHeading)) {
+//     heroHeading.style.opacity = "1";
+//     heroHeading.style.tranform = "translateY(-10)";
+//   }
+//   if (isInViewport(heroParagraph)) {
+//     heroParagraph.style.opacity = "1";
+//     heroParagraph.style.transform = "translateY(0)";
+//   }
+//   if (isInViewport(heroButton)) {
+//     heroButton.style.opacity = "1";
+//     heroButton.style.transform = "transalteY(0)";
+//   }
+
+//   //Animate features
+//   if (isInViewport(featureList)) {
+//     featureList.style.opacity = "1";
+//   }
+// });
